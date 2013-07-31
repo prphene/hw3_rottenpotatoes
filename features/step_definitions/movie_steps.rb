b561c89c96dfd2489931a9c807d10a9ffc232878
Pallavi Phene <prphene@gmail.com>
2013-07-31 04:56:13 +0000

    filter and sort changes

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if (!Movie.find_by_title(movie['title']))
+      Movie.create(movie)
+    end
   end
-  flunk "Unimplemented"
+#flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +17,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+#  page.body.should =~ /#{e1}<\/td>.*<td>#{e2}/
+  page.body.index(e1).should < page.body.index(e2)
+#flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +30,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+
+  rating_list.split(%r{,\s*}).each do |rating|
+    if uncheck 
+      step ('I uncheck "ratings['+rating+']"')
+    else
+      step ('I check "ratings['+rating+']"')
+    end
+  end
+end
+
+Then /I should see all of the movies/ do
+#  page.should have_css("#movies tr", :count=>10)
+  rows = page.all('#movies tr').size
+  rows.should == Movie.all.length + 1
+
 end
